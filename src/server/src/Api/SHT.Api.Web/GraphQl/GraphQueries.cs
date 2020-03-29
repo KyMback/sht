ad80196a06f78e4377017087451f3f4f66303c4f
KyMback <Seregick@hotmail.com>
2020-03-29 14:43:18 +0300

    Updated packages

--- src/server/src/Api/SHT.Api.Web/GraphQl/GraphQueries.cs
+++ src/server/src/Api/SHT.Api.Web/GraphQl/GraphQueries.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HotChocolate;
-using Microsoft.EntityFrameworkCore;
 using SHT.Application.Common;
 using SHT.Application.StateMachineConfigs.Core;
 using SHT.Application.Tests.StudentQuestions.Contracts;
